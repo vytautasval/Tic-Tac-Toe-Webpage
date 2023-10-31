6b4f6584b1cd75669ca99160a3c1c4b4717387dc
vytautasval <vytautasval40@gmail.com>
2023-10-31 13:19:18 +0200

    first commit

--- tic-tac-toe-webpage/js/script.js
+++ tic-tac-toe-webpage/js/script.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const cells = document.querySelectorAll('td') // Creates an array of all available cells.
-
-    addX(cells)
+    let isUserTurn = true
+    addX(cells, isUserTurn)
 })
 
-function addX(cells) {
+function addX(cells, isUserTurn) {
     cells.forEach(function(cell) {
         // Loops through each cell and adds an event listener to that specific cell.
         cell.addEventListener('click', function() {
-            cell.innerHTML = 'X'
+            if (cell.innerHTML === '' && isUserTurn === true) {
+                cell.innerHTML = 'X'
+                isUserTurn = false
+                setTimeout(function() {
+                    addO(cells)
+                    isUserTurn = true
+                }, 1000)
+            }
         })
     })
+}
+
+function addO(cells) {
+    const emptyCells = Array.from(cells).filter(cell => cell.innerHTML === '') //Array with unused cells
+    if (emptyCells.length > 0) {
+        //If there are empty cells a random location is chosen and O placed.
+        const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        randomCell.innerHTML = 'O'
+    }
 }
